384ce9ddbace596d2866619ba25b075f5205a023
Void-Roy <Roy.molendijk@hotmail.com>
2024-10-05 21:23:41 +0200

    Added Basestructure for Forest 2 Made small adaptions to Forest Tiles

---
+++ Scenes/Forest2/Forest_main2.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+const scene_type = 1
+#1 = map, 2 = battle, 3 = village?
+@export var min_lvl: int = 7
+@export var max_lvl: int = 14
+@export var world_enemies: Array [Enemy]
+@export var battle_bg: Texture
+@onready var _player_body = $Player
+var _spawned_npc  
+var spawn_request
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	AudioPlayer.play_music_level("res://musig/Dungeon_-_Catacomb_Crawler.ogg")
+	if sceneManager.last_scene == "res://Scenes/Battle/battle.tscn":
+		_player_body.position = player.position
+	if sceneManager.last_scene == "res://Scenes/Forest/Forest.tscn":
+		_player_body.position = $Forest2Exit.position + Vector2(-15,0)
+	if player.forest2_scene1 == 0:
+		$Player.disabled_spawn = true
+	MainMenu.map_scene = get_tree().current_scene
+
+func _input(_event):
+	pass
+
+func _on_forest2exit_body_entered(body: Node2D) -> void:
+	if body.name == _player_body.name:
+		sceneManager.goto_scene("res://Scenes/Forest/Forest.tscn")
