f2126698bbe30b3e1fb1d8ed5d95b3099f617745
Void-Roy <Roy.molendijk@hotmail.com>
2024-11-03 23:00:45 +0100

    .

--- Scenes/3dCave/3dboss_r.gd
+++ Scenes/3dCave/3dboss_r.gd
@@ -9,9 +9,9 @@ func _ready() -> void:
 		$Boss_R.queue_free()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+#func _process(_delta: float) -> void:
+	#pass
+#
 #func _on_body_entered(body: Node3D) -> void:
 	#player.enemy_encounter = enemy.resource_path
 	#player.position = _player_body.position
