a484122314122183d9909d2a4fc191812b89aafe
Void-Roy <Roy.molendijk@hotmail.com>
2024-11-14 19:21:20 +0100

    Changed spawn range for Cave.

--- Scenes/3dCave/3dcave_main.gd
+++ Scenes/3dCave/3dcave_main.gd
@@ -8,8 +8,8 @@ const scene_type = 1
 @export var max_lvl: int = 14
 @export var world_enemies: Array [Enemy]
 @export var battle_bg: Texture
-@export var min_spawn_range: int = 350
-@export var max_spawn_range: int = 450
+@export var min_spawn_range: int = 75
+@export var max_spawn_range: int = 125
 
 var spawn_request
 
