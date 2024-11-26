1bd3a97487d6b99e855930d1f573da8d1f520d25
viodaey <montgomeryy.br@gmail.com>
2024-11-26 13:20:59 +0100

    merge and fix

--- Global/npc3D.gd
+++ Global/npc3D.gd
@@ -84,7 +84,7 @@ func _physics_process(delta: float):
 func initiate_battle():
 	position = neutral_pos
 	player.enemy_encounter = []
-	player.enemy_encounter[0] = enemy.resource_path
+	player.enemy_encounter.append(enemy.resource_path)
 	player.position = player_position
 	sceneManager.mon_min_lvl = get_parent().min_lvl
 	sceneManager.mon_max_lvl = get_parent().max_lvl
