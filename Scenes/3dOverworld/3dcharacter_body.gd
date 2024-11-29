a4109418447e15f9669e59cf4fc4fda867e52d6b
Void-Roy <Roy.molendijk@hotmail.com>
2024-11-29 18:39:00 +0100

    Made changes to anim speed of wolves added Navigationmesh and Code in overworld.

--- Scenes/3dOverworld/3dcharacter_body.gd
+++ Scenes/3dOverworld/3dcharacter_body.gd
@@ -15,24 +15,30 @@ var in_scene: bool = false
 @onready var map = get_parent()
 @onready var raycast = $RayCast3D
 @onready var interactarea = $InteractArea
+@onready var navigation_agent = $NavigationAgent3D # Add NavigationAgent3D to your player
+@export var move_speed: float = 5.0
 var gravity: float
 
-func get_input():
+func get_input() -> Vector3:
 	var _input_direction = Input.get_vector("left", "right", "up", "down")
 	if not is_on_floor():
 		gravity = -0.1
 	else:
 		gravity = 0
-	velocity = Vector3(_input_direction.x, gravity, _input_direction.y) * speed
+	return Vector3(_input_direction.x, gravity, _input_direction.y) * speed
 
 func _ready() -> void:
 	move_dice = rng.randi_range(30, 100)
 	position.y = 0.2
+	navigation_agent.set_navigation_map(get_parent().get_node("NavigationRegion3D").get_navigation_map()) # Link NavigationMap
 
 func _physics_process(_delta):
 	if in_scene == true:
 		return
+
 	SimpleGrass.set_player_position(global_position)
+
+	# Handle Interactions
 	if Input.is_action_pressed("interact"):
 		if can_interact:
 			if interactarea.has_overlapping_areas():
@@ -49,64 +55,77 @@ func _physics_process(_delta):
 				await interactables[0].interact()
 				in_scene = false
 				can_interact = true
-	if Input.is_anything_pressed() == false:
-		_animated_sprite.play(("idle" + last_input))
-	elif Input.is_action_pressed(last_input):
-		_animated_sprite.play("move" + last_input)
-		moved = moved + 0.1
+
+	# Update Movement Animation
+	update_animation()
+
+	# Handle Player Movement
+	var target_velocity = get_input()
+	var target_position = global_transform.origin + target_velocity * _delta
+
+	# Check if position is reachable
+	if navigation_agent.is_target_reachable(target_position):
+		global_transform.origin = target_position
 	else:
-		if Input.is_action_pressed("up"):
-			interactarea.rotation_degrees.y = 180
-			_animated_sprite.play("moveup")
-			last_input = "up"
-			moved = moved + 0.1
-		if Input.is_action_pressed("down"):
-			interactarea.rotation_degrees.y = 0
-			_animated_sprite.play("movedown")
-			last_input = "down"
-			moved = moved + 0.1
-		if Input.is_action_pressed("right"):
-			interactarea.rotation_degrees.y = 90
-			_animated_sprite.play("moveright")
-			last_input = "right"
-			moved = moved + 0.1
-		if Input.is_action_pressed("left"):
-			interactarea.rotation_degrees.y = 270
-			_animated_sprite.play("moveleft")
-			last_input = "left"
-			moved = moved + 0.1
-	get_input()
+		print("Blocked by navigation boundary!")
 	move_and_slide()
 
-## 	spawn NPC	
-	if moved > move_dice and disabled_spawn == false:
+	# Handle NPC Spawning
+	if moved > move_dice and not disabled_spawn:
 		rng = RandomNumberGenerator.new()
-		for i in rng.randi_range(1,2):
-			var vec_target = await(_spawn_npc_loc())
-			get_node("RayCast3D").position = self.position
+		for i in rng.randi_range(1, 2):
+			var vec_target = _spawn_npc_loc()
+			raycast.position = self.position
 			raycast.target_position = raycast.position + vec_target
 			raycast.force_raycast_update()
 			if not raycast.is_colliding():
 				moved = 0
 				move_dice = rng.randi_range(25, 60)
-				var enemy_select = rng.randi_range(1,len(map.world_enemies))
+				var enemy_select = rng.randi_range(1, len(map.world_enemies))
 				map.spawn_request = load(map.world_enemies[enemy_select - 1].resource_path)
 				map.add_child(spawn_npc.instantiate().duplicate())
 				var num = len(activeSpawns)
 				var _spawned_npc = map.get_node("NPC_spawn")
-				_spawned_npc.name = "NPC_spawn" + "%d" %num
+				_spawned_npc.name = "NPC_spawn" + "%d" % num
 				_spawned_npc.position = self.position + vec_target
 				activeSpawns.append(_spawned_npc.name)
 
-func _spawn_npc_loc():
+func _spawn_npc_loc() -> Vector3:
 	rng = RandomNumberGenerator.new()
 	var angle = rng.randf_range(0, TAU)
 	var min_spawn_range = get_parent().min_spawn_range
 	var max_spawn_range = get_parent().max_spawn_range
 	var distance = rng.randi_range(min_spawn_range, max_spawn_range)
-	var vec_target = Vector3((distance)*cos(angle),0, (distance)*sin(angle))
-	return vec_target
+	return Vector3(distance * cos(angle), 0, distance * sin(angle))
 
 func _despawn_npc(npc):
 	map.get_node(npc).queue_free()
 	activeSpawns.erase(npc)
+
+func update_animation():
+	if Input.is_anything_pressed() == false:
+		_animated_sprite.play("idle" + last_input)
+	elif Input.is_action_pressed(last_input):
+		_animated_sprite.play("move" + last_input)
+		moved += 0.1
+	else:
+		if Input.is_action_pressed("up"):
+			interactarea.rotation_degrees.y = 180
+			_animated_sprite.play("moveup")
+			last_input = "up"
+			moved += 0.1
+		if Input.is_action_pressed("down"):
+			interactarea.rotation_degrees.y = 0
+			_animated_sprite.play("movedown")
+			last_input = "down"
+			moved += 0.1
+		if Input.is_action_pressed("right"):
+			interactarea.rotation_degrees.y = 90
+			_animated_sprite.play("moveright")
+			last_input = "right"
+			moved += 0.1
+		if Input.is_action_pressed("left"):
+			interactarea.rotation_degrees.y = 270
+			_animated_sprite.play("moveleft")
+			last_input = "left"
+			moved += 0.1
